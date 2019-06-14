25197650b52e575516a68a908d9e8fc954f25b8a
rainboy <rainboylvx@qq.com>
2019-06-14 17:27:21 +0800

    加入 led状态读取

--- keyboard/monster/driver/main.c
+++ keyboard/monster/driver/main.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
-#include<sys/types.h>
+#include <sys/types.h>
+#include <sys/time.h>
 #include<sys/stat.h>
 #include <fcntl.h>
 #include <wiringPi.h>
@@ -18,6 +19,8 @@
 
 
 fd_set rfds;
+struct timeval wait_for_wirite;
+struct timeval wait_for_read;
 
 int fd;
 static uint8_t  keyboard_led_stats = 0;
@@ -28,6 +31,7 @@ char hidg_path[] = "/dev/hidg0";
 
 /* host driver */ 
 static uint8_t keyboard_leds(void);
+static void read_keyboard_les(void);
 static void send_keyboard(report_keyboard_t *report);
 static void send_mouse(report_mouse_t *report);
 static void send_system(uint16_t data);
@@ -56,7 +60,6 @@ int main(){
     task_init();
     led_init();
 
-    FD_SET(fd, &rfds);
 
     host_set_driver(&pi_driver);
     printf("\n\nTMK: version 0.1 PI_ZERO started !\n");
@@ -64,18 +67,16 @@ int main(){
     keyboard_init();
     while(1){
         keyboard_task();
-        if (FD_ISSET(fd, &rfds)) {
-            char buf[8];
-            int led_status_len = read(fd,buf,7);
-            if( led_status_len){
-                keyboard_led_stats = buf[led_status_len-1];
-            }
-        }
+        read_keyboard_les();
     }
     return 0;
 }
 
 void task_init(){
+    wait_for_wirite.tv_sec = 0;
+    wait_for_wirite.tv_usec= 100;
+    wait_for_read.tv_sec = 0;
+    wait_for_read.tv_usec = 100;
 #ifndef NO_DEBUG
     debug_config.enable= 1;
     debug_config.matrix = 1;
@@ -94,6 +95,20 @@ static uint8_t keyboard_leds(void)
     return keyboard_led_stats;
 }
 //https://www.kernel.org/doc/Documentation/usb/gadget_hid.txt
+
+static void read_keyboard_les(){
+    FD_ZERO(&rfds);
+    FD_SET(fd, &rfds);
+    int retval = select(fd + 1, &rfds, NULL, NULL, &wait_for_read);
+    if (FD_ISSET(fd, &rfds) && retval > 0) {
+        char buf[8];
+        int led_status_len = read(fd,buf,7);
+        if( led_status_len){
+            keyboard_led_stats = buf[led_status_len-1];
+        }
+    }
+}
+
 static void send_keyboard(report_keyboard_t *report)
 {
 
@@ -101,11 +116,19 @@ static void send_keyboard(report_keyboard_t *report)
 /*#endif*/
     uint8_t timeout = 128;
     keyboard_report= *report;
-    int to_send = sizeof(report_keyboard_t);
-    if (write(fd, report, to_send) != to_send) {
-        perror(hidg_path);
-        return;
-    }
+    FD_ZERO(&rfds);
+    FD_SET(fd, &rfds);
+	int retval = select(fd + 1, NULL, &rfds, NULL, &wait_for_wirite);
+	if( retval > 0 ){
+        int to_send = sizeof(report_keyboard_t);
+        if (write(fd, report, to_send) != to_send) {
+            printf("wrong at write keycode at %s\n",hidg_path);
+            return;
+        }
+	}
+	else if( retval < 0){
+	    printf(" wirte key code reval = %d\n",retval);
+	}
 }
 
 static void send_mouse(report_mouse_t *report)
